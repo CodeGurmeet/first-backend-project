87a09292264953fa137d96cb0ff1fa64894817c3
CodeGurmeet <gurmeetsinghjabbal9167@gmail.com>
2025-08-18 20:11:34 +0530

    cors-cookie-configuration and utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config( {path : "./env"} );
-
+import { app } from "./app.js";
 import  connectDB  from "./db/index.js"
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error", (error)=>{
+        console.log("Error :", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server is listening on port ${process.env.PORT || 4000}`);
+    })
+})
+.catch((err)=>{
+    console.log("Database connection FAILED !!", err);
+    throw err
+})
 
 // An appraoch to connect the DB 
 /*
